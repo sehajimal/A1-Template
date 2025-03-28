e6803f3f6acdc8341def878e28a7c3103ea9d9fa
Sehaj <sehajimal@gmail.com>
2025-03-27 20:08:14 -0400

    Implemented folder structure for tests

---
+++ src/test/java/Tests.java
@@ -0,0 +1,12 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Tests {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
+
